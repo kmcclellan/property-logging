e9cb9d8625c95ba6174afac92ee09bf92c3438e5
Kyle McClellan <kylemcclellan3@gmail.com>
2022-01-13 08:16:25 -0500

    Tweak mapper contract
    * Explicit scope support
    * Non-generic interface

--- Logging.Properties/ILogPropertyMapper.cs
+++ Logging.Properties/ILogPropertyMapper.cs
@@ -3,10 +3,17 @@ namespace Microsoft.Extensions.Logging.Properties;
 using Microsoft.Extensions.Logging.Abstractions;
 
 /// <summary>
-/// Maps log information to named properties.
+/// Maps log information to named properties for a specific provider.
 /// </summary>
 /// <typeparam name="TProvider">The associated logger provider type.</typeparam>
-public interface ILogPropertyMapper<out TProvider>
+public interface ILogPropertyMapper<out TProvider> : ILogPropertyMapper
+{
+}
+
+/// <summary>
+/// Maps log information to named properties.
+/// </summary>
+public interface ILogPropertyMapper
 {
     /// <summary>
     /// Maps named properties for a log entry.
@@ -15,5 +22,11 @@ public interface ILogPropertyMapper<out TProvider>
     /// <param name="entry">The log entry.</param>
     /// <returns>The mapped properties.</returns>
     IEnumerable<KeyValuePair<string, object>> Map<TState>(LogEntry<TState> entry);
+
+    /// <summary>
+    /// Enables scope mapping using an external provider.
+    /// </summary>
+    /// <param name="scopes">The scope provider.</param>
+    void SetScopes(IExternalScopeProvider scopes);
 }
 
