afe3024b964452b2675e7d1fea3bfa0f6a241455
Kyle McClellan <kylemcclellan3@gmail.com>
2022-01-14 08:53:18 -0500

    Tweak mapper implementation
    * Pass scopes with each call

--- Logging.Properties/LogPropertyMapper.cs
+++ Logging.Properties/LogPropertyMapper.cs
@@ -9,26 +9,24 @@ class LogPropertyMapper<TProvider> : ILogPropertyMapper<TProvider>
 {
     readonly IEnumerable<ILogProperty> properties;
 
-    IExternalScopeProvider? scopes;
-
     public LogPropertyMapper(IEnumerable<ILogProperty> properties)
     {
         this.properties = properties;
     }
 
-    public IEnumerable<KeyValuePair<string, object>> Map<TState>(LogEntry<TState> entry)
+    public IEnumerable<KeyValuePair<string, object>> Map<TState>(
+        LogEntry<TState> entry,
+        IExternalScopeProvider? scopes = null)
     {
         foreach (var property in properties)
         {
             if (property.ProviderType == typeof(TProvider))
             {
-                foreach (var value in property.GetValues(entry, this.scopes))
+                foreach (var value in property.GetValues(entry, scopes))
                 {
                     yield return new(property.Name, value);
                 }
             }
         }
     }
-
-    public void SetScopes(IExternalScopeProvider scopes) => this.scopes = scopes;
 }
