9618ed3ee16d4dcb5fb7a92194405dfad5d7b9f4
Kyle McClellan <kylemcclellan3@gmail.com>
2022-08-19 12:56:48 -0400

    Abstract providers

---
+++ Logging.Properties/PropertyCollectingProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace Microsoft.Extensions.Logging.Properties;
+
+public abstract class PropertyCollectingProvider : ILoggerProvider, ISupportExternalScope
+{
+    IExternalScopeProvider? scopes;
+
+    public virtual void SetScopeProvider(IExternalScopeProvider scopes)
+    {
+        this.scopes = scopes;
+    }
+
+    public virtual ILogger CreateLogger(string category)
+    {
+        return this.CreateCollector(category).AsLogger(this.scopes);
+    }
+
+    public void Dispose()
+    {
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected abstract ILogPropertyCollector CreateCollector(string category);
+
+    protected virtual void Dispose(bool disposing)
+    {
+    }
+}
