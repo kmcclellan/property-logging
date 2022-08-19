b16071f34762490d915f480a8158a5188ec3fbb5
Kyle McClellan <kylemcclellan3@gmail.com>
2022-08-19 17:14:42 -0400

    Property mapping API

---
+++ Logging.Properties/LogPropertyCollectorExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace Microsoft.Extensions.Logging.Properties;
+
+public static class LogPropertyCollectorExtensions
+{
+    public static ILogEntryCollector Map(this ILogPropertyCollector collector, LogPropertyMappings mappings)
+    {
+        ArgumentNullException.ThrowIfNull(collector, nameof(collector));
+        ArgumentNullException.ThrowIfNull(mappings, nameof(mappings));
+
+        return new MappedCollector(collector, mappings);
+    }
+
+    private class MappedCollector : ILogEntryCollector
+    {
+        readonly ILogPropertyCollector collector;
+        readonly LogPropertyMappings mappings;
+
+        public MappedCollector(ILogPropertyCollector collector, LogPropertyMappings mappings)
+        {
+            this.collector = collector;
+            this.mappings = mappings;
+        }
+
+        public bool SkipMessage => throw new NotImplementedException();
+
+        public bool SkipProperties => throw new NotImplementedException();
+
+        public void AddMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProperty(string name, object? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Logging.Properties/LogPropertyMappings.cs
@@ -0,0 +1,5 @@
+﻿namespace Microsoft.Extensions.Logging.Properties;
+
+public class LogPropertyMappings
+{
+}
