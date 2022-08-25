982b6340f90902dccf76aa81f22b7d3c63a0b5a6
Kyle McClellan <kylemcclellan3@gmail.com>
2022-08-25 09:03:01 -0400

    Tweak collector options

--- Logging.Properties/PropertyCollectorOptions.cs
+++ Logging.Properties/PropertyCollectorOptions.cs
@@ -1,6 +1,10 @@
 namespace Microsoft.Extensions.Logging.Properties;
 
-public class PropertyCollectorOptions<TWriter, TProvider>
+public class PropertyCollectorOptions<TWriter>
 {
     public ICollection<LogWriteAction<TWriter>> Actions { get; } = new List<LogWriteAction<TWriter>>();
+}
+
+public class PropertyCollectorOptions<TWriter, TProvider> : PropertyCollectorOptions<TWriter>
+{
 }
